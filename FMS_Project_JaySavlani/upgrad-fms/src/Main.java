c5cca7dd8913f041bcd0b02abe5a9434d45f9db7
jay-Savlani <jaysavlani2608@gmail.com>
2021-09-07 18:35:21 +0530

    Changes done to the Main class and slight differences in UML

--- FMS_Project_JaySavlani/upgrad-fms/src/Main.java
+++ FMS_Project_JaySavlani/upgrad-fms/src/Main.java
@@ -1,27 +1,23 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
 public class Main {
 
+    //Print Ticket Details Method
 
+    public static String printTicketDetails(Ticket ticket) {
+        return ticket.getPnrNumber();
+    }
 
     public static void main(String[] args) {
-        Passenger p1 = new Passenger("Jay Savlani", "9904669996", "jaysavlani2608@gmail.com", "Rajeshwar Gold", "Vadodara", "Gujarat");
+        System.out.println("Welcome to Flight Reservation System");
 
-        System.out.println(p1.getContact().getContactDetails());
-        System.out.println(p1.getAddress().getAddressDetails());
-        System.out.println();
-
-        Flight f1 = new Flight(10, "Spice Jet", 250);
-        f1.book();
-        System.out.println(f1.getFlightDetails());
-
-        System.out.println();
+        Flight f1 = new Flight(01, "SpiceJet", 200);
         String locations[] = {"Juhu", "Haji Ali Dargah", "Gateway Of India", "Red Carpet Wax Museum", "Kidzania"};
-        TouristTickets ticket1 = new TouristTickets(f1, "235678", "Vadodara", "Mumbai", "23:00", "19:00", "07/09/2021", "07/09/2021", 45, 5000, "Hotel Paramount", locations);
-        System.out.println(ticket1.getTicketDetails());
-        System.out.println();
-        RegularTickets ticket2 = new RegularTickets(f1, "785678", "Vadodara", "Mumbai", "09:00", "08:00", "07/09/2021", "08/09/2021", 60, 5500, "Food, Water, Snacks");
-        System.out.println(ticket2.getTicketDetails());
+
+        TouristTickets ticket1 = new TouristTickets(f1, "335566", "Vadodara", "Mumbai", "21:00", "19:00", "7/09/2021", "7/09/2021", 45, 5000, "Hotel Paramount", locations);
+        RegularTickets ticket2 = new RegularTickets(f1, "445567", "Vadodara", "Mumbai", "21:00", "19:00", "7/09/2021", "7/09/2021", 45, 5000, "Food, Water, Snacks");
+
+        System.out.println(Main.printTicketDetails(ticket1));
+        System.out.println(Main.printTicketDetails(ticket2));
+
 
     }
 
